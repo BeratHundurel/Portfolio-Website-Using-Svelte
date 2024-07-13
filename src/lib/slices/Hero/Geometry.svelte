10b752f12e4d542d04eb550bfb63a66cbf230f85
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-07-13 21:48:29 +0300

    Adding bounce for first appearence

--- src/lib/slices/Hero/Geometry.svelte
+++ src/lib/slices/Hero/Geometry.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import { T as Threlte } from '@threlte/core';
-	import { Float } from '@threlte/extras';
+	import { createTransition, Float } from '@threlte/extras';
 	import * as THREE from 'three';
 	import gsap from 'gsap';
+	import { elasticOut } from 'svelte/easing';
+
 	export let position: [number, number, number] = [0, 0, 0];
 	export let geometry: THREE.BufferGeometry = new THREE.IcosahedronGeometry(3);
 	export let rate = 0.5;
+
+	let visible = false;
+	let reducedMotionRate = 0;
+
 	const materialParams = [
 		{ color: 0x2ecc71, roughness: 0 },
 		{ color: 0xf1c40f, roughness: 0.4 },
@@ -23,6 +29,33 @@
 		}
 		return new THREE.MeshStandardMaterial(gsap.utils.random(materialParams));
 	}
+
+	function handleCLick(event: MouseEvent) {
+		if ('object' in event && event.object instanceof THREE.Mesh) {
+			gsap.to(event.object.rotation, {
+				x: `+=${gsap.utils.random(0, 3)}`,
+				y: `+=${gsap.utils.random(0, 3)}`,
+				z: `+=${gsap.utils.random(0, 3)}`,
+				duration: 1.3,
+				ease: 'elastic.out(1,0.3)',
+				yoyo: true
+			});
+
+			event.object.material = getRandomMaterial();
+		}
+	}
+
+	const bounce = createTransition((ref) => {
+		return {
+			tick(t) {
+				if (t > 0) visible = true;
+				ref.scale.set(t, t, t);
+			},
+			easing: elasticOut,
+			duration: gsap.utils.random(800, 1200),
+			delay: gsap.utils.random(0, 500)
+		};
+	});
 </script>
 
 <Threlte.Group position={position.map((p) => p * 2)}>
@@ -32,6 +65,13 @@
 		rotationIntensity={6 * rate}
 		floatIntensity={5 * rate}
 	>
-		<Threlte.Mesh {geometry} material={getRandomMaterial()}></Threlte.Mesh>
+		<Threlte.Mesh
+			{visible}
+			in={bounce}
+			{geometry}
+			material={getRandomMaterial()}
+			interactive
+			on:click={handleCLick}
+		></Threlte.Mesh>
 	</Float>
 </Threlte.Group>
