d0eabffe0e26ca6757d2385092fa139101df52e1
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-07-16 11:19:00 +0300

    clean up

--- src/lib/slices/Hero/Shapes.svelte
+++ src/lib/slices/Hero/Shapes.svelte
@@ -22,25 +22,3 @@
 <ContactShadows position={[0, -3.5, 0]} opacity={0.65} scale={40} blur={1} far={9} />
 
 <AnimateRobot position={[0, 0, -3]} />
-<!-- Gem -->
-<!-- <Geometry position={[0, 0, 0]} rate={0.3} geometry={new THREE.IcosahedronGeometry(3)} /> -->
-
-<!-- Pill -->
-<!-- <Geometry
-	position={[3, 4, -2]}
-	rate={0.4}
-	geometry={new THREE.CapsuleGeometry(0.5, 1.6, 2, 16)}
-/> -->
-
-<!-- Soccer Ball -->
-<!-- <Geometry position={[-3, 4, -5]} rate={0.6} geometry={new THREE.DodecahedronGeometry(1.5)} /> -->
-
-<!-- Donut -->
-<!-- <Geometry
-	position={[-1.75, 0.5, 4]}
-	rate={0.5}
-	geometry={new THREE.TorusGeometry(0.6, 0.25, 16, 32)}
-/> -->
-
-<!-- Diamond -->
-<!-- <Geometry position={[-3, 4, -5]} rate={0.7} geometry={new THREE.OctahedronGeometry(1.5)} /> -->
